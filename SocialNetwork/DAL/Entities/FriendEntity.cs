b935d8a22741b74ac6ce2ddf15d274349193d7ee
visot <visotskiy.alexey@gmail.com>
2023-05-12 23:34:57 +0700

    Добавьте файлы проекта.

---
+++ SocialNetwork/DAL/Entities/FriendEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.DAL.Entities
+{
+    internal class FriendEntity
+    {
+        public int id { get; set; }
+        public int user_id { get; set; }
+        public int friend_id { get; set; }
+    }
+}
